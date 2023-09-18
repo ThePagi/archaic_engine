d74f87863096a9405d23450c9049203b3174759f
ThePagi <your@mother.com>
2023-09-18 20:39:53 +0200

    std time -> web-time

--- src/app.rs
+++ src/app.rs
@@ -5,7 +5,8 @@ use chrono::{DateTime, Local};
 use egui::Layout;
 use log::{info, warn, error};
 
-use std::{sync::mpsc, time::SystemTime};
+use std::sync::mpsc;
+use web_time::SystemTime;
 use style::*;
 
 pub const LOREM_IPSUM: &str = "Lorem 😏😏😏😏ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
